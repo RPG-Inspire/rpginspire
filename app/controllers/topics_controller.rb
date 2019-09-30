adc958761e19894eb623d9532e90b1b884eff9dd
Jose Jr <massa_@msn.com>
2019-09-30 23:22:45 +0800

    add topics#show controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,8 @@
 class TopicsController < ApplicationController
 
+  def show
+    @topic = Topic.eager_load(:user).find_by_slug!(params[:id])
+  end
   def new
     @topic = Topic.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :topics, only: [:new, :create]
+  resources :topics, only: [:show, :new, :create]
   namespace :api do
     resources :topics, only: [] do
       patch :upvote, on: :member
