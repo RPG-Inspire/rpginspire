3ef97d3f9928e3d5c32eb20aa8650181e9aa9568
Jose Jr <massa_@msn.com>
2019-10-02 04:51:02 +0800

    add sentry

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Rpginspire
     config.load_defaults 6.0
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
+    config.filter_parameters << :password
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
