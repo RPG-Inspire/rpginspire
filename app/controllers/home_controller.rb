01324cdb2b84eee9f2a6c1046874671bb8e57c59
Jose Jr <massa_@msn.com>
2019-10-06 23:06:29 +0800

    order topics by id desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @tags = Tag.all.order(title: :asc).group_by { |i| i.tag_type }
-    @topics = Topic.order(:id).page(params[:page]).per(20)
+    @topics = Topic.order(id: :desc).page(params[:page]).per(20)
   end
 
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
 
   def index
     @tags = Tag.all.order(title: :asc).group_by { |i| i.tag_type }
-    @topics = Topic.order(:id).page(params[:page]).per(20)
+    @topics = Topic.order(id: :desc).page(params[:page]).per(20)
   end
 
   def show
