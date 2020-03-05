cdb056767eb13e8be6a419a0587dea5b3e39da5c
Jose Jr <massa_@msn.com>
2020-03-05 10:00:26 +0800

    fix admin/tag permitted params
    https://sentry.io/organizations/rpginspire/issues/1538396966/?project=1767206&query=is%3Aunresolved

--- app/admin/tags.rb
+++ app/admin/tags.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register Tag do
+  permit_params :title, :tag_type
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :slug, :tag_type
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :slug, :tag_type]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :tag_type
+    end
+    f.actions
+  end
 end
+
